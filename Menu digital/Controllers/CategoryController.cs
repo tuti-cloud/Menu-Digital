e0705ef03e1d1a285ec8d39ef0f8a7df9005388d
k1tvrd <cmyakimavicius@gmail.com>
2025-10-17 16:13:56 -0300

    Controladores actualización

---
+++ Menu digital/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Menu_Digital.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+    }
+}

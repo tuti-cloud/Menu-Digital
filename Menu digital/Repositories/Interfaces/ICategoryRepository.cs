8b0a6f399ecc751cbcd490dfa4ed196051e39c3c
k1tvrd <cmyakimavicius@gmail.com>
2025-10-22 15:00:41 -0300

    Cambios cate

--- Menu digital/Repositories/Interfaces/ICategoryRepository.cs
+++ Menu digital/Repositories/Interfaces/ICategoryRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace Menu_Digital.Repositories.Interfaces
+﻿using Menu_Digital.Entities;
+
+namespace Menu_Digital.Repositories.Interfaces
 {
     public interface ICategoryRepository
     {
+        IEnumerable<Category> GetAll();
+        Category? GetCategoryById(int id);
+        Category Create(Category category);
+        void Update(Category category, int categoryId);
+        void Delete(int id);
+        public IEnumerable<Category> GetByrestaurantId(int restaurantId); //obtener categorias por restaurante
+
     }
 }
--- Menu digital/Repositories/Interfaces/IRestaurantRepository.cs
+++ Menu digital/Repositories/Interfaces/IRestaurantRepository.cs
@@ -1,12 +1,14 @@
-﻿namespace Menu_Digital.Repositories.Interfaces
+﻿using Menu_Digital.Entities;
+
+namespace Menu_Digital.Repositories.Interfaces
 {
     public interface IRestaurantRepository
     {
-
-        // Get all
-        // Get by id
-        // Update 
-        // delete
-        //create
+        IEnumerable <Restaurant> GetAll();
+        Restaurant? GetRestaurantById(int id);
+        Restaurant Create(Restaurant restaurant);
+        void Update(Restaurant updatedRestaurant, int restaurantId);
+        void Delete(int id);
+        public Restaurant? GetByEmail(string email); //para autenticación
     }
 }

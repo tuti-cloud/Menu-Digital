337618c3f2c9c9e7832cb025636e6a0b602d5f15
RegiSchiavo1 <reginadanielaschiavo@gmail.com>
2025-10-27 11:22:34 -0300

    cambios repositorios cate

--- Menu digital/Services/Interfaces/IProductService.cs
+++ Menu digital/Services/Interfaces/IProductService.cs
@@ -1,6 +1,16 @@
-﻿namespace Menu_Digital.Services.Interfaces
+﻿using System.Collections.Generic;
+using Menu_Digital.Models.DTOs.Requests;
+using Menu_Digital.Models.DTOs.Responses;
+
+namespace Menu_Digital.Services.Interfaces
 {
     public interface IProductService
     {
+        List<ProductDto> GetAllProducts();
+        ProductDto GetProductById(int id);
+        ProductDto Create(CreateProductRequest productDto);
+        ProductDto Update(int productId, CreateProductRequest productDto);
+        bool Delete(int productId);
     }
 }
+

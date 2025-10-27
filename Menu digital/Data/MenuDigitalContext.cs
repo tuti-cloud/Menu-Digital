2978a784c3c2152c71675fcd1f8e241239907fb9
k1tvrd <cmyakimavicius@gmail.com>
2025-10-27 11:10:33 -0300

    terminando repos

--- Menu digital/Data/MenuDigitalContext.cs
+++ Menu digital/Data/MenuDigitalContext.cs
@@ -8,7 +8,6 @@ namespace MenuDigital.Data
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-
         public DbSet<Restaurant> Restaurants { get; set; }
 
         public MenuDigitalContext(DbContextOptions<MenuDigitalContext> options) : base(options)
--- Menu digital/Repositories/Implementations/CategoryRepository.cs
+++ Menu digital/Repositories/Implementations/CategoryRepository.cs
@@ -2,39 +2,63 @@
 
 using Menu_Digital.Entities;
 using Menu_Digital.Repositories.Interfaces;
+using MenuDigital.Data;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 
 public class CategoryRepository : ICategoryRepository
 {
+    private readonly MenuDigitalContext _context; //contexto de la base de datos    
+    public CategoryRepository(MenuDigitalContext context)
+    {
+        _context = context;
+    }
+
     public Category Create(Category category)
     {
-        throw new NotImplementedException();
+        Category newcategory = _context.Categories.Add(category).Entity;
+        _context.SaveChanges();
+        return newcategory;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
-    }
-
-    public IEnumerable<Category> GetAll()
-    {
-        throw new NotImplementedException();
-    }
-
-    public IEnumerable<Category> GetByrestaurantId(int restaurantId)
-    {
-        throw new NotImplementedException();
+        var CategoryToDelete = _context.Categories.FirstOrDefault(c => c.Id == id); //busca la category con el id especificado
+        if (CategoryToDelete != null)
+        {
+            _context.Categories.Remove(CategoryToDelete); //si la encuentra, la elimina de la lista
+            _context.SaveChanges();
+        }
     }
 
     public Category? GetCategoryById(int id)
     {
-        throw new NotImplementedException();
+        return _context.Categories.FirstOrDefault(c => c.Id == id);
     }
 
-    public void Update(Category category, int categoryId)
+    public ICollection<Category> GetAll()
     {
-        throw new NotImplementedException();
+        return _context.Categories.ToList();
     }
+    ICollection<Category> ICategoryRepository.GetByrestaurantId(int restaurantId)
+    {
+        return _context.Categories
+        .Where(c => c.RestaurantId == restaurantId)
+        .ToList();
+    }
+
+    public void Update(Category updatedCategory, int categoryId)
+    {
+        Category? category = _context.Categories.SingleOrDefault(c => c.Id == categoryId);
+        if (category is not null)
+        {
+            category.Name = updatedCategory.Name;        
+
+            _context.SaveChanges();
+        }
+    }
+
+  
 }
 
 

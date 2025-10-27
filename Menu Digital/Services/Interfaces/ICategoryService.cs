731f977ed1a37ad02887a09abc4faf9615fcde54
RegiSchiavo1 <reginadanielaschiavo@gmail.com>
2025-10-27 11:14:21 -0300

    Interfaces

---
+++ Menu Digital/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using Menu_Digital.Models.DTOs.Requests;
+using Menu_Digital.Models.DTOs.Responses;
+
+namespace Menu_Digital.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        List<CategoryDto> GetAllCategories();
+        CategoryDto GetCategoryById(int id);
+        CategoryDto Create(CreateAndUpdateCategoryRequest request);
+        CategoryDto Update(int categoryId, CreateAndUpdateCategoryRequest categoryDto);
+        bool Delete(int categoryId);
+
+    }
+}

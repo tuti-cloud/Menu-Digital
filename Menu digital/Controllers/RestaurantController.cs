d9d9187e02e3969f71239c3a7e757b1e544c6f20
k1tvrd <cmyakimavicius@gmail.com>
2025-11-07 13:43:24 -0300

    controlador hecho

--- Menu digital/Controllers/RestaurantController.cs
+++ Menu digital/Controllers/RestaurantController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Menu_Digital.Models.DTOs.Requests;
+using Menu_Digital.Services.Implementation;
 using Menu_Digital.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace Menu_Digital.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[restaurants]")]
+    [Authorize]
     [ApiController]
     public class RestaurantController : ControllerBase
     {
@@ -13,12 +18,58 @@ namespace Menu_Digital.Controllers
         {
             _restaurantService = restaurantService;
         }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var restaurants = _restaurantService.GetAllRestaurants();
+            if (!restaurants.Any())
+                return NoContent();
+
+            return Ok(restaurants);
+        }
+
+        [HttpGet("{restaurantId}")]
+        public IActionResult GetRestaurantId(int restaurantId)
+        {
+            var restaurant = _restaurantService.GetByRestaurantId(restaurantId);
+
+            if (restaurant == null)
+            {
+                return NotFound($"Restaurante con ID {restaurantId} no fue encontrado.");
+            }
+
+            return Ok(restaurant);
+        }
+
+        [HttpPost]
+        public IActionResult CreateRestaurant(CreateAndUpdateRestaurantDto createRestaurantDto)
+        {
+            var newRestaurant = _restaurantService.Create(createRestaurantDto);
+
+            if (newRestaurant == null)
+            {
+                return BadRequest("No se pudo crear el restaurante.");
+            }
+
+            return Ok(newRestaurant);
+        }
+
+        [HttpDelete]
+        [Route("{restaurantId}")]
+        public IActionResult DeleteRestaurant(int restaurantId)
+        {
+            _restaurantService.Delete(restaurantId);
+            return NoContent();
+        }
+
+        [HttpPut]
+        [Route("{restaurantId}")]
+        public IActionResult UpdateRestaurant(CreateAndUpdateRestaurantDto dto, int restaurantId)
+        {
+            var updatedRestaurant = _restaurantService.Update(dto, restaurantId);
+            return Ok(updatedRestaurant);
+        }
     }
 }
-
-// RestaurantsController
-// [ApiController]
-// [Route("api/restaurants")]
-
-// GET /api/restaurants
 

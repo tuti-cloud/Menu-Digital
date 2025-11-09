7eb48f5d0516b7cddf4a4cc63dec69ab8a22b0df
k1tvrd <cmyakimavicius@gmail.com>
2025-11-09 14:47:39 -0300

    .

--- Menu Digital/Repositories/Interfaces/IRestaurantRepository.cs
+++ Menu Digital/Repositories/Interfaces/IRestaurantRepository.cs
@@ -12,5 +12,7 @@ namespace Menu_Digital.Repositories.Interfaces
         public Restaurant? GetByEmail(string email); //para autenticación
         Restaurant GetByName(string name);
 
+        ICollection<Restaurant> GetByProductName(string Name);
+
     }
 }

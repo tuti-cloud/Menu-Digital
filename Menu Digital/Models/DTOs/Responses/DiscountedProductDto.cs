64e0ca8cfd874a2f29117ca0e35b23928a755023
tuti-cloud <tutiangelini06@gmail.com>
2025-11-09 19:26:07 -0300

    Solucion problemas tecnicos

---
+++ Menu Digital/Models/DTOs/Responses/DiscountedProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Menu_Digital.Models.DTOs.Responses
+{
+    public class DiscountedProductDto
+    {
+        public string Name { get; set; }
+        public double DiscountPercentage { get; set; }   // admite 0–1 o 0–100
+        public decimal FinalPrice { get; set; }          // precio con descuento
+    }
+}
